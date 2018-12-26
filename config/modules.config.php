24d3f235cf347aba9a5c58a4ba1f4bc7681a7fb3
Dyuelber <dyuelbermiranda@outlook.com>
2018-12-26 10:53:40 -0200

    primeiro commit

---
+++ config/modules.config.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * List of enabled modules for this application.
+ *
+ * This should be an array of module namespaces used in the application.
+ */
+return [
+    'Zend\Session',
+    'Zend\Mvc\Plugin\Prg',
+    'Zend\Mvc\Plugin\Identity',
+    'Zend\Mvc\Plugin\FlashMessenger',
+    'Zend\Mvc\Plugin\FilePrg',
+    'Zend\Log',
+    'Zend\Form',
+    'Zend\Router',
+    'Zend\Validator',
+    'Application',
+];
