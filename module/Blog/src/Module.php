e012edfc5f39276ba745d1aa51a54b5f7e7f6f1c
Dyuelber <dyuelbermiranda@outlook.com>
2018-12-26 12:28:20 -0200

    Criacao do modulo Blog

---
+++ module/Blog/src/Module.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Blog;
+
+class Module {
+    const VERSION = '3.0.3-dev';
+
+    public function getConfig(){
+        return include __DIR__ . '/../config/module.config.php';
+    }
+}
